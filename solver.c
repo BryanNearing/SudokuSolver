f1021e5cf7f2584ca933a4c4bf1bed4f61e2d74c
BryanNearing <bryannearing@gmail.com>
2020-05-29 19:38:30 -0400

    initial commit

---
+++ solver.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int** convertToInt();
+extern char** readInGrid(char * filename);
+extern void printGrid(char** grid);
+
+int main(int argc, char * argv[]){
+
+
+	char grid** = malloc(;
+	
+	grid = readInGrid("puzzle.txt");
+	
+	printGrid(grid);
+	
+
+	return 0;
+}
+
+int** convertToInt(){
+
+	newGrid[9][9];
+	
+	return newGrid;
+
+}
+
+char** readInGrid(char * filename){
+
+	char grid[9][9];
+
+	FILE * puzzle;
+	puzzle = fopen("puzzle.txt", "r");
+	char c;
+	for(int i = 0; i < 9; i++){
+		for(int j = 0; j < 9; j++){
+			while ((c = getc(puzzle)) != EOF){
+  				grid[i][j] = c;
+  				printf("%c",  grid[i][j]);
+  			}
+  		}
+  	}
+  	
+  	return grid;
+
+}
+
+void printGrid(char** grid){
+
+	for(int i = 0; i < 9; i++){
+		for(int j = 0; j < 9; j++){
+			printf("%c ", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
