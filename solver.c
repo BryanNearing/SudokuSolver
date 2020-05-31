14ac4ac0f533417e53cedb31d315f297074114dd
BryanNearing <bryannearing@gmail.com>
2020-05-31 02:44:24 -0400

    remove conver to int because that already happens now

--- solver.c
+++ solver.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-extern void convertToInt();
 extern void readInFile(char * filename, int grid[]);
 extern void printGrid(int grid[]);
 extern int getNumberFromGrid(int row, int col, int * grid);
@@ -18,27 +17,6 @@ int main(int argc, char * argv[]){
 	
 
 	return 0;
-}
-
-void convertToInt(int grid[], char charGrid[]){
-	
-	for(int i = 0; i < 9; i++){
-		for(int j = 0; j < 9; j++){
-			int index = ((i * 9) +j);
-			if(charGrid[index] == ' '){
-				printf("-1\t");
-				grid[index] = -1;
-			}
-			else{
-				printf("%c\t", charGrid[index]);
-				int temp = charGrid[index];
-				printf("%d\t", temp);
-				grid[index] = charGrid[index];
-			}
-  		}
-  		printf("\n");
-  	}
-
 }
 
 void readInFile(char * filename, int grid[]){
